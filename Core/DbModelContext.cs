fe34a248d8a0124fd1b36e40d10107d2ad56238c
that-corych <woterblitzer2@gmail.com>
2023-03-28 11:49:15 +0300

    Добавьте файлы проекта.

---
+++ Core/DbModelContext.cs
@@ -0,0 +1,14 @@
+﻿using PDV.Task1._0.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDV.Task1._0.Core
+{
+    public static class DbModelContext
+    {
+        public static Task1PDVEntities1 DB { get; set; }
+    }
+}
